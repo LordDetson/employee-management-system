76b8dd06018261074b78a47e752c78dc0e5d9445
Dmitry Babanin <babanin.dima@gmail.com>
2021-01-11 17:03:20 +0300

    feat: Added Employee repository

---
+++ src/main/java/by/babanin/ems/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package by.babanin.ems.repository;
+
+import by.babanin.ems.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
