48d37d9583e48234882bd1aed077bb0bf1bf392e
Dmitry Babanin <babanin.dima@gmail.com>
2021-01-11 15:38:55 +0300

    Initialize Spring Boot application

---
+++ src/test/java/by/babanin/ems/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.babanin.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
