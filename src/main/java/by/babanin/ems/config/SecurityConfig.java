8c59ed118b0b53fcc2ec8266c81d8ed1c95e206d
Dmitry Babanin <babanin.dima@gmail.com>
2021-01-14 18:35:53 +0300

    config: Enable method security

--- src/main/java/by/babanin/ems/config/SecurityConfig.java
+++ src/main/java/by/babanin/ems/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import by.babanin.ems.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final EmployeeService employeeService;
